3a3a256329a6c09162445d5cd2df85d91c46cd36
Bruno Lima <brunolima.cad@gmail.com>
2020-10-07 23:51:31 -0600

    constants

---
+++ src/constants/constants.ts
@@ -0,0 +1,11 @@
+export enum RotDirection {
+   vertical = 'vertical',
+   horizontal = 'horizontal'
+}
+
+export enum GameStatus {
+   placeShips = 'place-ship',
+   player1Turn = 'player1-turn',
+   player2Turn = 'player2-turn',
+   attack = 'attack',
+}
