8e7d8bbcb060b051a39de8b94b9beac637df12c8
Bruno Lima <brunolima.cad@gmail.com>
2020-10-24 13:27:04 -0600

    major functionalities were brought a component higher

--- src/component/ships-menu/ShipsMenu.tsx
+++ src/component/ships-menu/ShipsMenu.tsx
@@ -1,86 +1,38 @@
-import React, { SyntheticEvent } from 'react';
-import { RotDirection, GameStatus } from '../../constants/constants';
-import MyButton from '../util/MyButton';
-import destroyer from '../../images/destroyer.jpg';
-import submarine from '../../images/submarine.jpg';
-import cruiser from '../../images/cruiser.jpg';
-import battleship from '../../images/battleship.jpg';
-import carrier from '../../images/carrier.jpg';
+import React from 'react';
 import { BsArrowRight } from 'react-icons/bs';
 
 interface Props {
-   setCurrentShipSize: React.Dispatch<React.SetStateAction<number>>,
-   rotateDirection: string,
-   setRotateDirection: React.Dispatch<React.SetStateAction<string>>,
-   setGameStatus: React.Dispatch<React.SetStateAction<string>>
+   setGameStatus: React.Dispatch<React.SetStateAction<string>>,
+   getShips: Function,
+   getRotateStyle: Function,
+   handleRotate: Function,
+   handleFinishButton: Function
 }
 
 const ShipsMenu = (props: Props) => {
 
-   function getShips() : React.ReactElement[] {
-      const buttons: React.ReactElement[] = [];
-      const sizes = [2, 3, 3, 4, 5];
-      const ships = [destroyer, submarine, cruiser, battleship, carrier];
-      let count = 1;
-      for (const _size of sizes) {
-         buttons.push((
-            <div key={'ship' + count}>
-               <MyButton 
-                  elClassName='myButton'
-                  size={_size}
-                  clickFunc={chooseShip}
-                  imgUrl={ships[count - 1]}
-               ></MyButton>
-            </div>
-         ));
-         count++;
-      }
-      return buttons;
-   }
-
-   function chooseShip(e: SyntheticEvent, size: number): void {
-      props.setCurrentShipSize(size);
-   }
-
-   function getRotateStyle() : any {
-      if (props.rotateDirection === RotDirection.horizontal)  {
-         return {
-            transform: 'rotate(0)'
-         }
-      } else {
-         return {
-            transform: 'rotate(90deg)'
-         }
-      }
-   }
-
-   function handleRotate() {
-      if (props.rotateDirection === RotDirection.horizontal)
-         props.setRotateDirection(RotDirection.vertical)
-      else
-         props.setRotateDirection(RotDirection.horizontal)
-   }
 
    return (
       <div className='ships-menu d-flex justify-content-around'>
          <div className='ships d-flex flex-column'>
             {
-               getShips()
+               props.getShips()
             }
          </div>
          <div className='rotate-btn d-flex flex-column align-items-center'>
             <div className='d-flex justify-content-around align-items-center'>
-               <div className='rotate btn btn-primary' onClick={handleRotate}>Rotate</div>
-               <div className='arrow-icon' style={getRotateStyle()}><BsArrowRight /></div>
+               <div className='rotate btn btn-primary' onClick={() => props.handleRotate()}>Rotate</div>
+               <div className='arrow-icon' style={props.getRotateStyle()}><BsArrowRight /></div>
             </div>
-            <div className='rotate btn btn-success' onClick={() => props.setGameStatus(GameStatus.attack)}>Finish</div>
+            <div 
+               className='rotate btn btn-success'
+               onClick={() => props.handleFinishButton()}
+            >
+               Finish
+            </div>
          </div>
       </div>
    )
-}
-
-const placeShipStyle = {
-   backgroundColor: '#ccc'
 }
 
 export default ShipsMenu
--- src/component/ships-menu/ShipsMenuContainer.tsx
+++ src/component/ships-menu/ShipsMenuContainer.tsx
@@ -1,23 +1,104 @@
-import React, { useContext } from 'react';
+import React, { useContext, SyntheticEvent } from 'react';
 import context from '../../context/Context';
 import ShipsMenu from './ShipsMenu';
+import { RotDirection, GameStatus } from '../../constants/constants';
+import MyButton from '../util/MyButton';
+import destroyer from '../../images/destroyer.jpg';
+import submarine from '../../images/submarine.jpg';
+import cruiser from '../../images/cruiser.jpg';
+import battleship from '../../images/battleship.jpg';
+import carrier from '../../images/carrier.jpg';
 
 import './ships-menu.css';
 
 const ShipsMenuContainer = ({ setGameStatus } : any) => {
 
-   const { setCurrentShipSize, rotateDirection, setRotateDirection } = useContext(context);
+   const { setCurrentShipSize, rotateDirection, setRotateDirection, gameStatus, occupiedSquares } = useContext(context);
+
+
+   /**
+    * It returns the 5 buttons containing each ship option
+    */
+   function getShips() : React.ReactElement[] {
+      const buttons: React.ReactElement[] = [];
+      const sizes = [2, 3, 3, 4, 5];
+      const ships = [destroyer, submarine, cruiser, battleship, carrier];
+      let count = 1;
+      for (const _size of sizes) {
+         buttons.push((
+            <div key={'ship' + count}>
+               <MyButton 
+                  elClassName='myButton'
+                  size={_size}
+                  clickFunc={chooseShip}
+                  imgUrl={ships[count - 1]}
+               ></MyButton>
+            </div>
+         ));
+         count++;
+      }
+      return buttons;
+   }
+   /**
+    * Sets the number of the choosen ship's size.
+    * With this information it's possible to make square validations
+    * 
+    * @param size    the size of the choosen ship
+    */
+   function chooseShip(e: SyntheticEvent, size: number): void {
+      setCurrentShipSize(size);
+   }
+
+
+   /**
+    * Changes the orientation of the ships to be placed onto the board
+    */
+   function getRotateStyle() : any {
+      if (rotateDirection === RotDirection.horizontal)  {
+         return {
+            transform: 'rotate(0)'
+         }
+      } else {
+         return {
+            transform: 'rotate(90deg)'
+         }
+      }
+   }
+
+   /**
+    * It changes the rotateDirection app context
+    */
+   function handleRotate() {
+      if (rotateDirection === RotDirection.horizontal)
+         setRotateDirection(RotDirection.vertical);
+      else
+         setRotateDirection(RotDirection.horizontal);
+   }
+
+   function handleFinishButton() {
+      if (occupiedSquares.length === 17) {
+         setGameStatus(GameStatus.attack);
+      } else {
+         alert('Set up your fleet first');
+      }
+   }
 
    return (
-      <div className='ships-menu-container'>
-         <h4>Select a ship</h4>
-         <ShipsMenu 
-            setCurrentShipSize={setCurrentShipSize}
-            rotateDirection={rotateDirection}
-            setRotateDirection={setRotateDirection}
-            setGameStatus={setGameStatus}
-         />
-      </div>
+      <>
+         {
+            gameStatus === GameStatus.placeShips &&
+            <div className='ships-menu-container'>
+               <h4>Select a ship</h4>
+               <ShipsMenu 
+                  setGameStatus={setGameStatus}
+                  getShips={getShips}
+                  getRotateStyle={getRotateStyle}
+                  handleRotate={handleRotate}
+                  handleFinishButton={handleFinishButton}
+               />
+            </div>
+         }
+      </>
    )
 }
 
